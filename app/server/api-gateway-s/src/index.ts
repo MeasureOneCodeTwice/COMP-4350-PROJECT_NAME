55526bbdfbe0b86964352e80909547aac7a2eb91
Roman Tebel <tebelroman@gmail.com>
2026-02-02 20:04:58 -0600

    API Gatewat Service: initialized with test api endpoint

---
+++ app/server/api-gateway-s/src/index.ts
@@ -0,0 +1,44 @@
+/*
+This service should have express, cors and http-proxy-middleware installed as dependencies.
+It will act as an API Gateway, routing requests to the appropriate microservices. In the real world, the other 
+ microservices would not be reachable from the outside internet, but for now this will do.
+
+The /api/test endpoint will try to reach every other stood up service and return their status to client.
+
+*/
+
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { json } from 'stream/consumers';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+interface Dictionary {
+  [key: string]: string;
+}
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`API Gateway running on port ${PORT} at http://localhost:${PORT}`);
+});
+
+//test endpoint
+app.get('/api/test', (req: express.Request, res: express.Response) => {
+    console.log('Received test request at API Gateway: ', req.url);
+    let output: Dictionary = {};
+
+    output['api-gateway'] = `ok`;
+    let services: string[] = ['analytics', 'user', 'authentication', 'market'];
+
+    for (let i = 1; i < 5; i++) {
+        // reach out to each microservice
+        const service = services[i - 1];
+        if (service) {
+            output[service] = `http://localhost:300${i}/api/test`;
+        }
+    }
+    res.json(output);
+});
